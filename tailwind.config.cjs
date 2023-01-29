1782fd4db5b6b9dbd3c9e582249c76b6b594f307
Daizaikun <leo-im@hotmail.com>
2023-01-29 01:19:10 -0500

    leenaiden-web

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      Paprika: ["Paprika", "cursive"],
+    },
+  },
+  plugins: [],
+};
